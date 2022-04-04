087ff5d59be8c891c2d044afc2779daabdb50a28
Moumen.exe <ourmoumen@gmail.com>
2022-04-04 12:06:48 +0200

    i add nav bar and style it and make it resposive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var navbtn = document.querySelector("[data-navbtn]")
+console.log(navbtn)
+var dropdown = document.querySelector(" [data-dropdown] ")
+
+
+navbtn.addEventListener('click' , ()=>{
+  console.log('helloo')
+    dropdown.classList.remove
+    dropdown.classList.add("dropdown-items-active")
+  
+})
